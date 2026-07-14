802d4ee1a873ba5bf3abc0831873f3ca9e92395d
Git user <gituser@none>
2026-07-14 14:01:38 -0700

    tdiff: fix coding style

--- tdiff.c
+++ tdiff.c
@@ -1,18 +1,18 @@
-/* Test program for gcov-diff: two code paths based on argv.
-   Compile with -g -O2, run with perf record -b, then gcov.py to profile. */
+/* Test program for gcov-diff: two code paths based on argv. */
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
-    int fast = (argc > 1 && strcmp(argv[1], "fast") == 0);
-    volatile long sum = 0;
-    for (int i = 0; i < 1000000; i++) {
-        if (fast) {
-            sum += i;
-        } else {
-            sum += i * i;
-        }
-    }
-    printf("sum = %ld\n", sum);
-    return 0;
+int main(int argc, char **argv)
+{
+	int fast = (argc > 1 && strcmp(argv[1], "fast") == 0);
+	volatile long sum = 0;
+	for (int i = 0; i < 1000000; i++) {
+		if (fast) {
+			sum += i;
+		} else {
+			sum += i * i;
+		}
+	}
+	printf("sum = %ld\n", sum);
+	return 0;
 }
